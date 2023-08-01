7119fa60b53f1b2a192b26af328293c1e4e499e7
Ikanor1221 <ilya.bochkov97@gmail.com>
2023-08-01 20:37:54 +0300

    Setup the project structure with bpm, Webpack, Eslint

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+// import * as path from "path";
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
